2c555b183c365c694759243c78470a12c60045c9
花歹 <gatsbywang@126.com>
2017-05-08 22:57:53 +0800

    解决http请求两个问题：1、json解析 2、公共参数

--- framelibrary/src/main/java/com/library/framelibrary/HttpCallback.java
+++ framelibrary/src/main/java/com/library/framelibrary/HttpCallback.java
@@ -2,7 +2,9 @@ package com.library.framelibrary;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
 import com.library.baselibrary.http.EngineCallback;
+import com.library.baselibrary.http.HttpUtils;
 
 import java.util.Map;
 
@@ -10,10 +12,10 @@ import java.util.Map;
  * Created by 花歹 on 2017/5/7.
  * Email:   gatsbywang@126.com
  * Description: 带有业务的回调
- * Thought:
+ * Thought:1、解决公共参数 2、解决Json解析
  */
 
-public abstract class HttpCallback implements EngineCallback {
+public abstract class HttpCallback<T> implements EngineCallback {
     @Override
     public final void onPreExecute(Context context, Map<String, String> header, Map<String, Object> params) {
         //请求前的做的事情，如公共参数（公共参数涉及到了业务，公共参数的添加不能写到HttpUtils里）
@@ -24,4 +26,13 @@ public abstract class HttpCallback implements EngineCallback {
     public void onPreExecute() {
 
     }
+
+    @Override
+    public final void onSuccess(String result) {
+        Gson gson = new Gson();
+        T t = (T) gson.fromJson(result, HttpUtils.analysisClazzInfo(this));
+        onSuccess(t);
+    }
+
+    public abstract void onSuccess(T result);
 }
